adbeec9e982f030e5e711b3b5cfac8c9510c8b15
Grany35 <ogun.ergin35@gmail.com>
2023-01-07 20:04:28 +0300

    Added Fonts & Splash-screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+        <Text>hi</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
