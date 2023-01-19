ce2a09d202ea57a9c5e3e8eeed5b64f13726494f
Grany35 <ogun.ergin35@gmail.com>
2023-01-20 00:40:41 +0300

    Added HeaderButton in ChatListScreen

--- screens/ChatListScreen.tsx
+++ screens/ChatListScreen.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/CustomHeaderButton";
 
 const ChatListScreen = (props) => {
+  useEffect(() => {
+    props.navigation.setOptions({
+      headerRight: () => {
+        return (
+          <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+            <Item title="New Chat" iconName="create-outline" onPress={() => {}} />
+          </HeaderButtons>
+        );
+      },
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>ChatListScreen</Text>
